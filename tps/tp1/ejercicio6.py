7079537617c11b2912ab7cbf01c85a8ab07c8a95
lucasfozzati <fozzatti98@gmail.com>
2020-03-26 21:45:37 -0300

    tp1

--- tps/tp1/ejercicio6.py
+++ tps/tp1/ejercicio6.py
@@ -2,5 +2,11 @@ print('escriba una lista de numero separados: ', end='')
 numeros = list(map(int, input().split()))
 
 print(numeros)
+
+numeros.sort()
+
+print(numeros)
+
 numeros.reverse()
+
 print(numeros)
